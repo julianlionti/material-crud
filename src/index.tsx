ed9e0aad675622b341721b25cad3144a2cb41107
Julian <info@racaps.com>
2020-09-16 23:20:08 -0300

    sin comentario

--- src/index.tsx
+++ src/index.tsx
@@ -21,16 +21,3 @@ export {
   llamarWS,
   useWindowSize,
 }
-// export * as Form from './components/Formulario'
-// export * as CenteredCard from './components/UI/CenteredCard'
-// export * as Dialog from './components/UI/Dialog'
-// export * as ABMContext from './utils/ABMContext'
-// export * as useAxios from './utils/useAxios'
-// export * as useWindowSize from './utils/useWindowSize'
-// export const sarasa = () => {
-//   return 'Vamo Racing'
-// }
-
-// module.exports = {
-//   CRUD: require('./components/ABM')
-// }
