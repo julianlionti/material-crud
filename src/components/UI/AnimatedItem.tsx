07e13b1fce002dea4d85f9a88cd4de377409eae1
Julian <info@racaps.com>
2020-09-28 16:25:06 -0300

    terminado

--- src/components/UI/AnimatedItem.tsx
+++ src/components/UI/AnimatedItem.tsx
@@ -8,14 +8,35 @@ interface Props {
 }
 
 export default memo(({ children, edited, deleted }: Props) => {
-  console.log(edited)
+  const classes = useClasses()
 
   return (
-    <div>
-      <span>Prueba</span>
-      <Collapse timeout={2000} in={!deleted} unmountOnExit>
-        {children}
-      </Collapse>
-    </div>
+    <Collapse
+      className={`${edited ? classes.edited : ''}`}
+      timeout={1000}
+      in={!deleted}
+      unmountOnExit>
+      {children}
+    </Collapse>
   )
 })
+
+const useClasses = makeStyles(() => ({
+  edited: {
+    animation: '$shake 0.5s',
+    animationIterationCount: 1,
+  },
+  '@keyframes shake': {
+    '0%': { transform: 'translate(1px, 1px) rotate(0deg)' },
+    '10%': { transform: 'translate(-1px, -2px) rotate(-1deg)' },
+    '20%': { transform: 'translate(-3px, 0px) rotate(1deg)' },
+    '30%': { transform: 'translate(3px, 2px) rotate(0deg)' },
+    '40%': { transform: 'translate(1px, -1px) rotate(1deg)' },
+    '50%': { transform: 'translate(-1px, 2px) rotate(-1deg)' },
+    '60%': { transform: 'translate(-3px, 1px) rotate(0deg)' },
+    '70%': { transform: 'translate(3px, 1px) rotate(-1deg)' },
+    '80%': { transform: 'translate(-1px, -1px) rotate(1deg)' },
+    '90%': { transform: 'translate(1px, 2px) rotate(0deg)' },
+    '100%': { transform: 'translate(1px, -2px) rotate(-1deg)' },
+  },
+}))
