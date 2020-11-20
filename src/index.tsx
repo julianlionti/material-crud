b8981589cd75edfd36d955f77d12d3434981cac8
Julian <info@racaps.com>
2020-11-20 16:20:03 -0300

    con exports

--- src/index.tsx
+++ src/index.tsx
@@ -17,8 +17,9 @@ import useAxios, { callWs } from './utils/useAxios'
 import useWindowSize from './utils/useWindowSize'
 
 export type { CustomComponentProps } from './components/Form/AlCustom'
-export type { UseAxiosProps } from './utils/useAxios'
+export type { UseAxiosProps, CallProps, Error, ErrorResponse } from './utils/useAxios'
 export type { OpcionesProps } from './components/Form/FormTypes'
+export type { RefProps, CrudProps } from './components/Crud/index'
 
 export {
   Crud,
