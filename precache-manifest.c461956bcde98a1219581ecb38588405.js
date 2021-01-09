8a9193d0a82085eaca572a08d9f9b0cb47731aad
Julian <info@racaps.com>
2021-01-09 17:23:29 -0300

    Updates

---
+++ precache-manifest.c461956bcde98a1219581ecb38588405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0272a6cd77bfe0c597e75d97f9412c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d4366ecc5f5712be632",
+    "url": "./static/js/2.951dd013.chunk.js"
+  },
+  {
+    "revision": "4e579e46dcb97b0b58fa9ad3f4aed0e5",
+    "url": "./static/js/2.951dd013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e33d3fc2a88b9b86e48",
+    "url": "./static/js/main.26eed453.chunk.js"
+  },
+  {
+    "revision": "20f9cb35124502fcdc4d",
+    "url": "./static/js/runtime-main.8e7e5ab1.js"
+  }
+]);
